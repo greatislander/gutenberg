688a9346ebe822d21516208d36a01f5c9d8a5559
Ned Zimmerman <ned@bight.ca>
2018-12-09 13:56:47 -0600

    Add run-theme-tests script

---
+++ bin/run-theme-tests.sh
@@ -0,0 +1,31 @@
+# Exit if any command fails
+set -e
+
+# Check for theme input, or set to default
+if [ -z "$1" ]; then
+	THEME="twentynineteen"
+else
+	THEME="$1"
+fi
+
+# Change to the expected directory
+cd "$(dirname "$0")/../"
+
+# Setup local environement
+( ./bin/setup-local-env.sh )
+
+# Install and activate theme
+if [ "$THEME" != "twentynineteen" ]; then
+	docker-compose $DOCKER_COMPOSE_FILE_OPTIONS run -T --rm cli theme install $THEME --activate
+fi
+
+# Get theme name
+THEME_NAME=$(docker-compose $DOCKER_COMPOSE_FILE_OPTIONS run -T --rm cli theme get $THEME --field=title)
+
+if [ "$E2E_ROLE" = "author" ]; then
+	echo "Testing $THEME_NAME...\n"
+	# WP_PASSWORD=authpass WP_USERNAME=author npm run test-theme
+else
+	echo "Testing $THEME_NAME...\n"
+	# npm run test-theme
+fi
